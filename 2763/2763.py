d70ee93abc603414ea76f6c29b2034cc9969dca2
wender zilo <wyndy19@hotmail.com>
2021-01-04 21:11:01 -0300

    Day 03 - URI 2763

---
+++ 2763/2763.py
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+value = input("")
+one_value = value.split(".")[2]  
+ 
+print(value.split(".")[0])
+print(value.split(".")[1])
+print(one_value.split("-")[0])
+print(value.split("-")[-1])
