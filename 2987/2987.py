63718736e85592b3857f9ad0019c1dd632631e92
wender zilo <wyndy19@hotmail.com>
2021-01-06 23:38:25 -0300

    Day 05 - uri 2987

---
+++ 2987/2987.py
@@ -0,0 +1,7 @@
+entrada = input("")
+
+alfabeto = ["","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"]
+resultado = alfabeto.index(entrada)
+
+print(resultado)
+
