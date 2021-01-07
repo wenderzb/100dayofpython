5af5d136cda699ca41915eab09d68eaea31a06b1
wender zilo <wyndy19@hotmail.com>
2021-01-07 00:00:09 -0300

    Day 05 - uri 2987 v2

--- 2987/2987.py
+++ 2987/2987.py
@@ -1,6 +1,6 @@
 entrada = input("")
 
-alfabeto = ["","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"]
+alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 resultado = 0
 
 for item in alfabeto:
